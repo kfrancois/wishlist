fe1ab48571408b69009d086342de47404f144d31
billgiot <bill.giotofficial@gmail.com>
2017-11-08 18:40:33 +0100

    template10 project maken

---
+++ WishList/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using WishList.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace WishList.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}

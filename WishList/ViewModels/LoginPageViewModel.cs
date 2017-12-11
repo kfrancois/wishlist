f5d6d2006ee444aeafe215bc828e1a418ae18bfe
PrebenLeroy <preben.leroy@live.be>
2017-12-11 23:01:01 +0100

    Set pivot in mainpage

--- WishList/ViewModels/LoginPageViewModel.cs
+++ WishList/ViewModels/LoginPageViewModel.cs
@@ -9,16 +9,6 @@ namespace WishList.ViewModels
 {
     public class LoginPageViewModel : ViewModelBase
     {
-        /*public LoginPageViewModel()
-        {
-            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
-            {
-                Value = "Designtime value";
-            }
-        }
-
-        string _Value = "Testing";
-        public string Value { get { return _Value; } set { Set(ref _Value, value); } }*/
 
         public void GotoMainPage() =>
             NavigationService.Navigate(typeof(Views.MainPage));

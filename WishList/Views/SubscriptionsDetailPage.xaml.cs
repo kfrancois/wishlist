3aca4f844b0a962051e9fd8a1aff34fffdabf6c4
PrebenLeroy <preben.leroy@live.be>
2017-12-17 11:16:20 +0100

    Add contentdialog

--- WishList/Views/SubscriptionsDetailPage.xaml.cs
+++ WishList/Views/SubscriptionsDetailPage.xaml.cs
@@ -46,5 +46,30 @@ namespace WishList.Views
                 this.WishListItem.Add(wish);
             }
         }
+
+        private async void ButtonShowMessageDialog_Click(object sender, RoutedEventArgs e)
+        {
+
+            var dialog = new Windows.UI.Popups.MessageDialog(
+                        "Are you sure you want to fulfill this Wish?",
+                        "Fulfill this Wish");
+
+            dialog.Commands.Add(new Windows.UI.Popups.UICommand("Yes") { Id = 0 });
+            dialog.Commands.Add(new Windows.UI.Popups.UICommand("No") { Id = 1 });
+
+            /*if (Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily != "Windows.Mobile")
+            {
+                // Adding a 3rd command will crash the app when running on Mobile !!!
+                dialog.Commands.Add(new Windows.UI.Popups.UICommand("Maybe later") { Id = 2 });
+            }*/
+
+            dialog.DefaultCommandIndex = 0;
+            dialog.CancelCommandIndex = 1;
+
+            var result = await dialog.ShowAsync();
+
+            /*var btn = sender as Button;
+            btn.Content = $"Result: {result.Label} ({result.Id})";*/
+        }
     }
 }
